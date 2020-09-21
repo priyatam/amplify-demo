fa4042ee291efbf23d48f3b78ee528212043f6b9
Priyatam Mudivarti <mudivarp@amazon.com>
2020-09-21 15:38:27 -0700

    updated generated files from new amplify version

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,69 +1,87 @@
-// eslint-disable
+/* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateProduct = `subscription OnCreateProduct {
-  onCreateProduct {
-    id
-    name
-    description
-    price
-    image
-  }
-}
-`;
-export const onUpdateProduct = `subscription OnUpdateProduct {
-  onUpdateProduct {
-    id
-    name
-    description
-    price
-    image
-  }
-}
-`;
-export const onDeleteProduct = `subscription OnDeleteProduct {
-  onDeleteProduct {
-    id
-    name
-    description
-    price
-    image
-  }
-}
-`;
-export const onCreateUser = `subscription OnCreateUser {
-  onCreateUser {
-    id
-    username
-    avatar {
-      bucket
-      region
-      key
+export const onCreateProduct = /* GraphQL */ `
+  subscription OnCreateProduct {
+    onCreateProduct {
+      id
+      name
+      description
+      price
+      image
+      createdAt
+      updatedAt
     }
   }
-}
 `;
-export const onUpdateUser = `subscription OnUpdateUser {
-  onUpdateUser {
-    id
-    username
-    avatar {
-      bucket
-      region
-      key
+export const onUpdateProduct = /* GraphQL */ `
+  subscription OnUpdateProduct {
+    onUpdateProduct {
+      id
+      name
+      description
+      price
+      image
+      createdAt
+      updatedAt
     }
   }
-}
 `;
-export const onDeleteUser = `subscription OnDeleteUser {
-  onDeleteUser {
-    id
-    username
-    avatar {
-      bucket
-      region
-      key
+export const onDeleteProduct = /* GraphQL */ `
+  subscription OnDeleteProduct {
+    onDeleteProduct {
+      id
+      name
+      description
+      price
+      image
+      createdAt
+      updatedAt
     }
   }
-}
+`;
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      username
+      avatar {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      username
+      avatar {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      username
+      avatar {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
 `;
